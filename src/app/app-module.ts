133513a736deaef0f4c1270eb297218f271c8607
Guilherme Corrêa Narcizo <guilherme.narcizo@fatec.sp.gov.br>
2025-10-10 15:41:50 -0300

    :ok_hand: import and exports removed

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -5,21 +5,16 @@ import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { SharedModule } from "./modules/shared/shared-module";
-import { SelectComponent } from "./modules/shared/components/select/select.component";
 
 @NgModule({
   declarations: [
     App,
-    SelectComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgbModule,
     SharedModule // módulo para exibir os componentes compartilhados
-  ],
-  exports: [
-    SelectComponent
   ],
   providers: [
     provideBrowserGlobalErrorListeners()
