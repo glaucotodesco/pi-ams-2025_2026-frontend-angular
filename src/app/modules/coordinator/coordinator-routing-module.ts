f76b3089dcdf99f72a36893c54b40ab55ad4cb86
Yuri Alves <yuri.alves.silva@gmail.com>
2025-10-13 14:57:58 -0300

    :sparkles: feat: ensure integration of navbar preconfigured modules routes

--- src/app/modules/coordinator/coordinator-routing-module.ts
+++ src/app/modules/coordinator/coordinator-routing-module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { MenuService } from '../shared/menu-service';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: Home, // rota /admin/home renderiza o componente Home
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class CoordinatorRoutingModule { }
+export class CoordinatorRoutingModule {
+  constructor(private menuService: MenuService) {
+    this.menuService.addMenu([
+      { label: 'Início', path: '/coordinator/' },
+      {
+        label: 'Usuários',
+        children: [
+          { label: 'Lista', path: '/coordinator/users/list' },
+          { label: 'Criar', path: '/coordinator/users/create' },
+        ],
+      },
+    ]);
+  }
+}
--- src/app/modules/sdudent/sdudent-module.ts
+++ src/app/modules/sdudent/sdudent-module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StudentRoutingModule } from './student-routing-module';
+import { Home } from './pages/home/home';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    Home
+  ],
   imports: [CommonModule, StudentRoutingModule],
 })
 export class SdudentModule {}
