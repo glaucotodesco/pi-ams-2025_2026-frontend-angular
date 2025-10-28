75decc71280f8e1a355e9caa4e6ee582ae382c2c
FelipePaterni <felipesp.chaves@gmail.com>
2025-10-28 13:07:11 -0300

    💥 fix: revert changes on app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,4 +2,21 @@ import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './
+  templateUrl: './app.html',
+  standalone: false,
+  styleUrls: ['./app.css']
+})
+export class App {
+  protected readonly title = signal('pi-ams-2025_2026-frontend-angular');
+  salvar() {
+  alert('Função de ação executada!');
+}
+onModalConfirmed() {
+  console.log("Modal confirmado");
+}
+
+onModalCancelled() {
+  console.log("Modal cancelado");
+}
+
+}
